58d9fa58ac990b934c0f5d5be361b46be8d4bda7
Biazus, Miller <miller@miller-VirtualBox.(none)>
2013-02-28 13:52:13 +0100

    First colored pixel location foud

--- bmp-parser-text/test.cpp
+++ bmp-parser-text/test.cpp
@@ -3,15 +3,17 @@
 #include <vector>
 #include "bmp.h"
 
+using namespace std;
 
-std::vector<char> bmp_file_buffer;
+
+vector<char> bmp_file_buffer;
 PBITMAPFILEHEADER bmp_file_header;
 PBITMAPINFOHEADER bmp_info_header;
 int bmp_data_offset;
 
 
 void fillBpmBuffer() {
-    std::ifstream file("/home/tim/Desktop/test.bmp");
+    std::ifstream file("/home/miller/test.bmp");
 
     if (file) {
         file.seekg(0,std::ios::end);
@@ -23,7 +25,7 @@ void fillBpmBuffer() {
 
         bmp_file_header = (PBITMAPFILEHEADER)(&bmp_file_buffer[0]);
         bmp_info_header = (PBITMAPINFOHEADER)(&bmp_file_buffer[0] + sizeof(BITMAPFILEHEADER));
-        bmp_data_offset = sizeof(BITMAPFILEHEADER) + sizeof(PBITMAPINFOHEADER);
+        bmp_data_offset = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
         
     }
 
@@ -33,18 +35,23 @@ fillBpmBuffer();
 
 // get the pointer to the first element in the array, add 10 (10 byte offset) re-cast to int* (pointer to int), dereference to get the value
 int bmpOffset = *(int*) ( &bmp_file_buffer[0] + 10);
+cout << bmpOffset << endl;
+cout << sizeof(unsigned int) << endl;
+cout << sizeof(char) << endl;
+cout << "bmp_data_offset: "<< bmp_data_offset << endl;
+cout << (int) bmp_file_buffer[bmp_data_offset] << endl;
+cout <<  sizeof(bmp_file_buffer) << endl;
 
 
-std::cout << bmpOffset << std::endl;
-
-
-std::cout << sizeof(unsigned int) << std::endl;
-std::cout << sizeof(char) << std::endl;
-
-
-std::cout << bmp_data_offset << std::endl;
-
-std::cout << (int) bmp_file_buffer[bmp_data_offset] << std::endl;
+//transverses the array of pixels
+for(int i=bmp_data_offset; i < bmp_file_buffer.size(); i++){
+	cout << (int) bmp_file_buffer[i] << endl;
+	
+	//comparison: acceps if it has a color
+	if((int) bmp_file_buffer[bmp_data_offset]){
+		return 0;
+	}
+}
 
 return 0;
 
